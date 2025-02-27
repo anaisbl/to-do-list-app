b1b2cb978a8424170ec2f8e329cded789dbf2f9d
Anais <131727001+nanadess@users.noreply.github.com>
2025-02-27 12:05:21 +0100

    Update app.py
    Basic Add task buttons, event and input

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLineEdit, QVBoxLayout, QWidget, QListWidget
 
 # core of every Qt app is the QApplication class
 # every app needs one QApplication object to function
@@ -10,21 +9,44 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
 
 # good approach is to subclass the main window and self contain its behavior
 
-class MainWindow(QMainWindow):
+class toDoApp(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("To do list")
-        button = QPushButton("Idk what this does")
 
-        # set central widget of the window
-        self.setCentralWidget(button)
+        self.setWindowTitle("To do list")
+        self.setGeometry(100,100,400,300)
+
+        self.tasks = []
+
+        self.layout = QVBoxLayout()
+
+        self.input_field = QLineEdit()
+        self.add_button = QPushButton("Add Task")
+        self.task_list = QListWidget()
+
+        self.layout.addWidget(self.input_field)
+        self.layout.addWidget(self.add_button)
+        self.layout.addWidget(self.task_list)
+
+        self.add_button.clicked.connect(self.add_task)
+
+        self.setLayout(self.layout)
+    
+    def add_task(self):
+        task = self.input_field.text()
+        if task:
+            self.tasks.append(task)
+            self.task_list.addItem(task)
+            self.input_field.clear()
+    
+
 
 # Qapplication instance
 # passing sys.argv argument to allow CL arguments for the app
 app = QApplication(sys.argv)
 
 # create Qtwidget which is our window
-window = MainWindow()
+window = toDoApp()
 window.show()   # because windows are hidden by default
 
 # start event loop
